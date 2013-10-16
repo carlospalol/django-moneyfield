ebd47049815823f7531063b7d348b732a1f5c39b
Carlos Palol <carlos.palol@awarepixel.com>
2013-10-16 14:59:29 +0200

    Test settings as a separate module

--- tests/runtests.py
+++ tests/runtests.py
@@ -6,24 +6,31 @@ import sys
 from django.conf import settings
 from django.core import management
 
+# test_settings = {
+#     'DATABASES': {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#         }
+#     },
+#     'USE_L10N': True,
+#     'SECRET_KEY': "justthetestapp",
+#     'INSTALLED_APPS': (
+#         'testapp',
+#     ),
+#     'MONEY_CURRENCY_CHOICES': (
+#             ('AAA', 'AAA'),
+#             ('BBB', 'BBB'),
+#             ('CCC', 'CCC'),
+#         )
+# }
+
 def main():
     testing_dir = os.path.abspath(os.path.dirname(__file__))
-    package_dir = os.path.normpath(testing_dir + '/../')
+    package_dir = os.path.normpath(os.path.join(testing_dir, os.pardir))
     sys.path.append(testing_dir)
     sys.path.append(package_dir)
     
-    settings.configure(**{
-        'DATABASES': {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-            }
-        },
-        'USE_L10N': True,
-        'SECRET_KEY': "justthetestapp",
-        'INSTALLED_APPS': (
-            'testapp',
-        ),
-    })
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
     management.call_command('test', 'testapp')
 
 if __name__ == "__main__":
---
+++ tests/test_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+USE_L10N = True
+SECRET_KEY = "justthetestapp"
+INSTALLED_APPS = (
+    'testapp',
+)
+MONEY_CURRENCY_CHOICES = (
+    ('AAA', 'AAA'),
+    ('BBB', 'BBB'),
+    ('CCC', 'CCC'),
+)
