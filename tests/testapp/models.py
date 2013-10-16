1303e1a1b3cc4726fcdd86526f1fc85808b8abde
Carlos Palol <carlos.palol@awarepixel.com>
2013-10-16 15:00:01 +0200

    No need for unrelated model fields

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -3,9 +3,7 @@ from moneyfield import MoneyField
 
 
 class Book(models.Model):
-    name = models.CharField(max_length=100)
     price = MoneyField(decimal_places=2, max_digits=8, currency='EUR')
 
 class Translator(models.Model):
-    name = models.CharField(max_length=100)
     fee = MoneyField(decimal_places=2, max_digits=8)
