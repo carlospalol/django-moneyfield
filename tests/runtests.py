9f7def5114aa4c065b10c69b439b3a98eddd2609
Carlos Palol <carlos.palol@awarepixel.com>
2013-10-18 15:23:18 +0200

    Clean up

--- tests/runtests.py
+++ tests/runtests.py
@@ -6,23 +6,6 @@ import sys
 from django.conf import settings
 from django.core import management
 
-# test_settings = {
-#     'DATABASES': {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#         }
-#     },
-#     'USE_L10N': True,
-#     'SECRET_KEY': "justthetestapp",
-#     'INSTALLED_APPS': (
-#         'testapp',
-#     ),
-#     'MONEY_CURRENCY_CHOICES': (
-#             ('AAA', 'AAA'),
-#             ('BBB', 'BBB'),
-#             ('CCC', 'CCC'),
-#         )
-# }
 
 def main():
     testing_dir = os.path.abspath(os.path.dirname(__file__))
@@ -32,6 +15,7 @@ def main():
     
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
     management.call_command('test', 'testapp')
+
 
 if __name__ == "__main__":
     main()
